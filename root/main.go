2953b8ad6f6f3aaaf60673061e8aeb87a16b188e
Wisney <wisneymaciel2@hotmail.com>
2018-07-08 02:22:30 -0300

    trying fixing the conflicts again '-'

--- root/main.go
+++ root/main.go
@@ -11,7 +11,6 @@ import (
 	"os/signal"
 	"rpg/assets"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -188,11 +187,44 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Print(err)
 	}
 
-	page := strings.Replace(string(formSignin), "{{.Message}}", "E aew os caras! e.e", 1)
+	fullData := map[string]interface{}{
+		"NavigationBar": template.HTML(navigationBarHTML),
+		"Page":          template.HTML(formSignin),
+	}
+	render(w, r, homepageTpl, "homepage_view", fullData)
+}
+
+// createAccountHandler renders the homepage view template
+func createAccountHandler(w http.ResponseWriter, r *http.Request) {
+	push(w, "/static/style.css")
+	push(w, "/static/navigation_bar.css")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	formSignup, err := ioutil.ReadFile("./pages/form_signup.html")
+	if err != nil {
+		fmt.Print(err)
+	}
 
 	fullData := map[string]interface{}{
 		"NavigationBar": template.HTML(navigationBarHTML),
-		"Page":          template.HTML(page),
+		"Page":          template.HTML(formSignup),
+	}
+	render(w, r, homepageTpl, "homepage_view", fullData)
+}
+
+func characterHandler(w http.ResponseWriter, r *http.Request) {
+	push(w, "/static/style.css")
+	push(w, "/static/navigation_bar.css")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	characterSheet, err := ioutil.ReadFile("./pages/character_sheet.html")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fullData := map[string]interface{}{
+		"NavigationBar": template.HTML(navigationBarHTML),
+		"Page":          template.HTML(characterSheet),
 	}
 	render(w, r, homepageTpl, "homepage_view", fullData)
 }
